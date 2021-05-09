5ad3f5af99e100e01d328a725055310cf2ab7b3c
AndreiDebreceni <81584248+AndreiDebreceni@users.noreply.github.com>
2021-05-09 17:10:17 +0300

    Add files via upload

---
+++ java/MyFirstTest.java
@@ -0,0 +1,11 @@
+public class MyFirstTest {
+
+
+
+
+        public static void main(String[] args) {
+            LoginTest lt = new LoginTest();
+            lt.loginWithValidCredentials();
+        }
+
+}
